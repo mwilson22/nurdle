7b84c2674f091f9f278e0fdf16c789d3ff8030fb
Mark Wilson <toques-nightly.0c@icloud.com>
2022-12-08 14:31:26 +0000

    Add 0.0.0.0 to the allowed hosts (playing the game on a mobile on the same network)

--- nurdle_proj/settings.py
+++ nurdle_proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(=l3b+4m653t!#)m+podq6bs87!22dgbr=*)bobq$b)z8k%bxx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.214', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.1.214', 'localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
